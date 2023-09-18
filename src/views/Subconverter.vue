8b999c91913e0430ac140d9b13082ed680a3ea6a
NatsukiKoo <39581263+AlterGu@users.noreply.github.com>
2023-09-18 11:14:08 +0800

    Update Subconverter.vue
    add myself conver domain

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -210,7 +210,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://subconverter-h6cu.onrender.com/sub?" },{ value: "https://subconverter.alterkoo.tk/sub?" }],
         remoteConfig: [
           {
             label: "universal",
